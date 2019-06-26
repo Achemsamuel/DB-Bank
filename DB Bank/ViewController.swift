e4783d98723a6111dbf8c54c7e5173aa6a163d57
Achem Samuel <achemsamuel@gmail.com>
2019-06-26 11:58:12 +0100

    Initial Commit

---
+++ DB Bank/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DB Bank
+//
+//  Created by Achem Samuel on 6/26/19.
+//  Copyright © 2019 Achem Samuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DB BankTests/DB_BankTests.swift
@@ -0,0 +1,34 @@
+//
+//  DB_BankTests.swift
+//  DB BankTests
+//
+//  Created by Achem Samuel on 6/26/19.
+//  Copyright © 2019 Achem Samuel. All rights reserved.
+//
+
+import XCTest
+@testable import DB_Bank
+
+class DB_BankTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
