933c9c295adb65e5ea2ddd38b948d07f66ca527b
Achem Samuel <achemsamuel@gmail.com>
2019-06-29 14:38:42 +0100

    implement popover feature for Dashboard VC

---
+++ DB Bank/Controller/CardViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CardViewController.swift
+//  DB Bank
+//
+//  Created by Achem Samuel on 6/29/19.
+//  Copyright © 2019 Achem Samuel. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var handleArea: UIView!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
